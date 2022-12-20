84d2df6df9d9667abed1d625c4b9b9d5af81b8f1
David Kirby <david@kinetic.com>
2022-12-20 15:37:51 -0600

    preparing to convert to wp

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+    <title>Mass Effect Fansite V.2</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+<link rel="preconnect" href="https://fonts.bunny.net">
+<link href="https://fonts.bunny.net/css?family=chonburi:400" rel="stylesheet" />
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm text-capitalize p-0">
+    
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="" class="nav-link">link</a>
+            </li>
+            <li class="nav-item">
+            <a href="" class="nav-link">link</a>
+            </li>
+            <li class="nav-item">
+            <a href="" class="nav-link">link</a>
+            </li>
+        </ul>
+</nav>
+
+<header class="d-flex p-4 bg-img" 
+style="background-image: url('images/headbg.jpg'); 
+background-size: 100% 100%;">
+
+    <img src="images/N7logo.jpg" alt="logo">
+    <h1 class="text-capitalize mx-auto">mass effect fansite</h1>
+
+</header>
+
+<div class="container-fluid d-flex flex-column min-vh-100 p-0 bg-img"
+style="background-image: url('images/space.webp');
+background-size: cover;">
+
+<main class="container-fluid flex-grow-1">
+
+<h2>intro</h2>
+
+</main>
+</div>
+
+<footer class="d-flex text-capitalize">
+    <a href="http://">contact</a>
+    <p>author: david kirby</p>
+</footer>
+    
+</body>
+</html>
